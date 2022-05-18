ba52b669c21b3304007148b20e5a4bc398e631ae
anastasiya-pv <99739456+anastasiya-pv@users.noreply.github.com>
2022-05-19 00:11:37 +0300

    Create HW_2.1.sql

--- HW_2.1.sql
+++ HW_2.1.sql
@@ -166,4 +166,95 @@ values (default,3,7),
        (default,14,7),
        (default,115,3),
        (default,10,1);
-      
+       
+--Таблица roles
+--Создать таблицу roles
+-- id. Serial  primary key,
+-- role_name. int, not null, unique
+--Поменять тип столба role_name с int на varchar(30)
+--Наполнить таблицу roles 20 строками:
+
+create table roles(
+id Serial  primary key,
+role_name Int not null unique);
+
+alter table roles alter column role_name type varchar(30) using role_name::varchar(30); 
+
+insert into roles(role_name)
+values ('Junior Python developer'),
+       ('Middle Python developer'),
+       ('Senior Python developer'),
+       ('Junior Java developer'),
+       ('Middle Java developer'),
+       ('Senior Java developer'),
+       ('Junior JavaScript developer'),
+       ('Middle JavaScript developer'),
+       ('Senior JavaScript developer'),
+       ('Junior Manual QA engineer'),
+       ('Middle Manual QA engineer'),
+       ('Senior Manual QA engineer'),
+       ('Project Manager'),
+       ('Designer'),
+       ('HR'),
+       ('CEO'),
+       ('Sales manager'),
+       ('Junior Automation QA engineer'),
+       ('Middle Automation QA engineer'),
+       ('Senior Automation QA engineer');
+
+--Таблица roles_employee
+--Создать таблицу roles_employee
+-- id. Serial  primary key,
+-- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+--Наполнить таблицу roles_employee 40 строками:
+
+create table roles_employee(
+id serial primary key,
+employee_id Int not null unique,
+role_id Int not null,
+foreign key (role_id)
+references roles(id));
+
+insert into roles_employee(id,employee_id,role_id)
+values (default,7,2),
+       (default,20,4),
+       (default,3,9),
+       (default,5,13),
+       (default,23,4),
+       (default,11,2),
+       (default,10,9),
+       (default,22,13),
+       (default,21,3),
+       (default,34,4),
+       (default,6,7),
+       (default,70,2),
+       (default,29,1),
+       (default,15,19),
+       (default,18,11),
+       (default,9,1),
+       (default,64,5),
+       (default,51,6),
+       (default,14,8),
+       (default,17,12),
+       (default,50,3),
+       (default,48,7),
+       (default,66,1),
+       (default,12,9),
+       (default,44,13),
+       (default,37,5),
+       (default,27,16),
+       (default,68,17),
+       (default,55,9),
+       (default,19,4),
+       (default,47,18),
+       (default,4,19),
+       (default,26,11),
+       (default,46,13),
+       (default,69,4),
+       (default,52,16),
+       (default,33,7),
+       (default,36,8),
+       (default,8,4),
+       (default,49,10);
+       
